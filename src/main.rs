43d6036c8f0c8f5399cd2e8d7141871d65b30d2c
Misaka15535 <46892084+Misaka15535@users.noreply.github.com>
2022-05-06 00:41:07 +0800

    config

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use std::env;
 use std::env::{args, current_dir, current_exe};
 use std::ffi::OsStr;
+use std::fs::remove_file;
 use std::iter::once;
 use std::os::windows::ffi::OsStrExt;
+use std::path::PathBuf;
 use std::process::exit;
 use std::ptr::null_mut;
 
-use ini::Ini;
+use ini::{Ini};
+use itertools::Itertools;
 use winapi::ctypes::c_int;
 use winapi::shared::minwindef::UINT;
 use winapi::um::winuser::{IDCANCEL, MB_OK, MB_OKCANCEL};
@@ -66,57 +70,68 @@ fn main() {
     let main = MainWindow::new();
     let lol_path = read_lol_path();
     main.on_click(move || {
-        lol_path;
+        // lol_path;
 
-        check_ini();
+        check_ini(&lol_path);
     });
     main.run();
 }
 
 const LOL_EXE: &str = "Client.exe";
+const START_TIP: &str = r#"本程序有两种打开方式:
+最简单的办法就是把LOL的快捷方式或者是启动程序“Client.exe”拖放到本程序的图标上，本程序会把自身拷贝到C盘根目录下，并创建桌面快捷方式。需要删除时只需要删除桌面快捷方式和C盘根目录下的文件就可以完全删除;
+如果你不喜欢文件在你的C盘根目录下创建文件，你才需要在你认为合适的目录下直接打开本程序，进行手动指定LOL启动程序文件路径该方式会在本程序文件同目录下创建"\#open_lol.ini"配置文件;
+按下确定键，程序讲继续运行，你需要手动选择LOL启动程序文件路径；
+按下取消键，程序将退出，你可以将LOL的快捷方式或者是启动程序“Client.exe”拖放到本程序的图标上以重新打开本程序；
+\n\n
+祝你游戏愉快！"#;
 
 fn read_lol_path() -> String {
     if args().count() > 0 {
-        let path = args().find_or_first(|arg| {
+        let path = env::args().find_or_first(|arg| {
             arg.contains(LOL_EXE)
         }).unwrap_or_else(|| {
             message_box("未识别的文件或命令行", MB_OK);
             exit(0);
         });
-        check_current_exe_path(&path);
+        check_current_exe_path();
         return path;
     }
     let conf_path = current_dir().unwrap().join("open_lol.ini");
-    if !conf_path.exists() {
-        let message_code = message_box("本程序有两种打开方式，最简单的办法就是把LOL的快捷方式或者是\
-        启动程序“Client.exe”拖放到本程序的图标上，本程序会把自身拷贝到C盘根目录下，并创建桌面快捷方式。需要删除时只需\
-        要删除桌面快捷方式和C盘根目录下的文件就可以完全删除。\n\n\
-        如果你不喜欢文件在你的C盘根目录下创建文件，你才需要在你认为合适的目录下直接打开本程序，进行手动指定LOL启动程序文件路径\
-        该方式会在本程序文件同目录下创建\"open_lol.in\"配置文件。\n\n\
-        按下确定键，程序讲继续运行，你需要手动选择LOL启动程序文件路径；\n
-        按下取消键，程序将退出，你可以将LOL的快捷方式或者是启动程序“Client.exe”拖放到本程序的图标上以重新打开本程序。\
-        \n\n
-        祝你游戏愉快！\
-        ", MB_OKCANCEL);
-        if message_code == IDCANCEL {
+    return match conf_path.exists() {
+        true => {
+            let mut conf = Ini::load_from_file(&conf_path).unwrap();
+            if let Some(prop) = conf.section(Some("lol")) {
+                if let Some(path) = prop.get("path") {
+                    return path.to_string();
+                }
+            }
+            // 没有读入就是无效数据段
+            remove_file(&conf_path);
+            check_path_ini(&conf_path).unwrap()
+        }
+        false => {
+            check_path_ini(&conf_path).unwrap()
+        }
+    }
+}
+
+fn check_path_ini(conf_path: &PathBuf) -> Option<String> {
+    let message_code = message_box(START_TIP, MB_OKCANCEL);
+    if message_code == IDCANCEL {
+        exit(0);
+    }
+    let path = match open_file_dialog("请选择LOL启动程序", vec![FileFilter::new("请选择LOL启动程序(Client.exe)", vec![LOL_EXE])]) {
+        Some(path) => { path.to_str()?.trim_end_matches(LOL_EXE).to_string() }
+        None => {
+            message_box("玩不起就别玩！！！", MB_OK);
             exit(0);
         }
-        let path = match open_file_dialog("请选择LOL启动程序", vec![FileFilter::new("请选择LOL启动程序(Client.exe)", vec![LOL_EXE])]) {
-            Some(path) => { path.to_str().unwrap().to_string() }
-            None => {
-                message_box("玩不起就别玩！！！", MB_OK);
-                exit(0);
-            }
-        };
-        let mut conf = Ini::new();
-        conf.with_section(Some("lol")).set("exe", &path);
-        conf.write_to_file(conf_path);
-        return path;
-    }
-    let conf = Ini::load_from_file(conf_path).unwrap();
-
-    let section_lol = conf.section(Some("lol")).unwrap();
-    section_lol.get("exe").unwrap().to_string()
+    };
+    let mut conf = if conf_path.exists() { Ini::load_from_file(conf_path).unwrap() } else { Ini::new() };
+    conf.with_section(Some("lol")).set("path", &path);
+    conf.write_to_file(conf_path);
+    return Some(path);
 }
 
 fn message_box(msg: &str, btn_type: UINT) -> c_int {
@@ -132,8 +147,27 @@ fn message_box(msg: &str, btn_type: UINT) -> c_int {
 }
 
 /// 检查当前exe目录是否正确
-fn check_current_exe_path(path: &String) {}
+fn check_current_exe_path() {
+    println!("{:?}",current_exe().unwrap());
+    println!("{:?}",PathBuf::from("c:\\".to_owned() + current_exe().unwrap().file_name().unwrap().to_str().unwrap()));
+    if current_exe().unwrap() == PathBuf::from("c:\\".to_owned() + current_exe().unwrap().file_name().unwrap().to_str().unwrap()) {
+        message_box("exe==", MB_OK);
+    } else {
+        message_box("exe!=", MB_OK);
+    }
+}
 
-fn check_ini() {
-    println!("{:?}", current_exe().unwrap());
+fn check_ini(path: &String) {
+    let ini_path = path.to_owned() + "wegame_launch.ini";
+    println!("{:?}", &ini_path);
+    let mut conf = if PathBuf::from(&ini_path).exists() { Ini::load_from_file(&ini_path).unwrap() } else { Ini::new() };
+    if let Some(prop) = conf.section(Some("TCLS")) {
+        if let Some(data_name) = prop.get("data_name") {
+            if data_name == "lol" {
+                return;
+            }
+        }
+    }
+    conf.with_section(Some("")).set("data_name", "lol");
+    conf.write_to_file(ini_path);
 }
